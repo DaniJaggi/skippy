351145ed74734d42cfea5e575990befe98cf17fc
Dani Jaggi <daniel.jaggi@aarenet.com>
2016-04-05 18:20:32 +0200

    check for collisions

--- src/js/skippy.js
+++ src/js/skippy.js
@@ -24,11 +24,9 @@
     var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(0.4, 0.4, 0.5);
-    var actionStone;
     
     var cameraPos = new BABYLON.Vector3(0,dim.man,6-dim.length/2);
     var camera = new BABYLON.FreeCamera("camera1", cameraPos, scene);
-    //var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, cameraPos, scene);
     camera.setTarget(new BABYLON.Vector3(0,0,18-dim.length/2));
     camera.attachControl(canvas, false);
 
@@ -57,20 +55,20 @@
 	var baseBody;
 	var stones = [];
 	var idCount = 0;
-	var rotationDelta = 0.001;
-	var speedDelta = 0.0005;
-	
+	var rotationDelta = 0.0005;
+	var speedDelta = 0.008;
+	var curlDelta = 0;//0.001;
+    var rotationTakeover = 0.4;
+    
 	function Stone(id) {
 		var that = this;
 		that.id = id; 
 		that.red = (id % 2)==0;
-		that.position = new BABYLON.Vector3(0,0.2+0.1*id,0);
+		that.position = new BABYLON.Vector3(0,0.001+0.001*id,0);
 		that.visible = false;
-		var rotation = 0;
-		var direction = 0;
-		var speed = 0;
-		
-		var body = baseBody.createInstance("b"+id);
+		that.state = { s:0, d:0, r:0 }
+        
+ 		var body = baseBody.createInstance("b"+id);
 		body.position = that.position;
 		body.isVisible = false;
 		
@@ -91,37 +89,65 @@
 			shade.isVisible = that.visible;
 		}
 		
-		that.move = function() {
+		that.move = function(sec) {
 			if (!that.visible) return;
-			if (rotation>rotationDelta) {
-				handle.rotation.y += rotation;
-				rotation -= rotationDelta;
-			} else if (rotation<-rotationDelta) {
-				handle.rotation.y += rotation;
-				rotation += rotationDelta;
-			}				
-			if (speed>speedDelta) {
-				that.position.x -= Math.sin(direction)*speed;
-				that.position.z -= Math.cos(direction)*speed;
-				speed -= speedDelta;
+            
+         	if (that.state.r>rotationDelta) {
+				handle.rotation.y += that.state.r;
+				that.state.r -= rotationDelta;
+                that.state.d += curlDelta;
+			} else if (that.state.r<-rotationDelta) {
+				handle.rotation.y += that.state.r;
+				that.state.r += rotationDelta;
+                that.state.d -= curlDelta;
 			}
-            if (that===actionStone) {
-                camera.setTarget(that.position);
+			if (that.state.s>speedDelta) {
+				that.position.x -= Math.sin(that.state.d)*that.state.s*sec;
+				that.position.z -= Math.cos(that.state.d)*that.state.s*sec;
+				that.state.s -= speedDelta;
+			}
+            for (var p=id+1;p<16;p++) {
+                var impuls = stones[p].colision(that.position,that.state);
+                if (impuls) {
+                    that.state.d = impuls.d;
+                    that.state.s = impuls.s;
+                    that.state.r = impuls.r; 
+                }
             }
 		}
 				
 		that.push = function(d,s,r) {
-			direction = d;
-			speed = s;
-			rotation = r;
+			that.state.d = d;
+			that.state.s = s;
+			that.state.r = r;
 		}
+        
+        that.colision = function(p,s) {
+            if (!that.visible) return;
+            var dx = that.position.x-p.x;
+            var dz = that.position.z-p.z;
+            var dist = Math.sqrt(dx*dx+dz*dz);
+            if (dist<1) {
+                var a1 = Math.atan(dx/dz);
+                var a2 = a1-s.d;
+                var ns1 = s.s*Math.sin(a2);
+                var nd1 = a1-Math.PI/2;
+                var nr1 = s.r*(1-rotationTakeover);
+                
+                var ns2 = s.s*Math.cos(a2);
+                var nd2 = a1;
+                var nr2 = s.r*rotationTakeover;
+                
+                that.state.s = ns2;
+                that.state.d = nd2;
+                that.state.r = nr2;
+                
+                console.log("@@ nd1="+nd1+",   ns1="+ns1);
+                
+                return { d:nd1, s:ns1, r:nr1 };
+            }
+        }
 		
-	}
-	
-	function distance(id1,id2) {
-		var dx = stones[id1].position.x-stones[id2].position.x;
-		var dy = stones[id1].position.z-stones[id2].position.z;
-		return Math.sqrt(dx*dx+dy*dy);
 	}
 	
     
@@ -152,9 +178,13 @@
 			stones[i] = new Stone(i);
 		}
 		
+        var lastMove = BABYLON.Tools.Now;
 		scene.registerBeforeRender(function() {  
-			for (var i=0;i<16;i++) {
-			    stones[i].move();
+			var now = BABYLON.Tools.Now;
+            var sec = (now-lastMove)/1000.0;
+            lastMove = now;
+            for (var i=0;i<16;i++) {
+			    stones[i].move(sec);
 			}
         });
 		startup();
@@ -165,18 +195,46 @@
     });
 	
 	function startup() {
-        actionStone = stones[0];
-        
+        camera.lockedTarget = stones[0];
+
 		stones[0].setPosition(0,dim.tee1+20-dim.length/2);
-		stones[0].push(0,0.14,0.18);
+		stones[0].push(0/*dir*/,3.5/*fps*/,0.28);
 		
-		stones[1].setPosition(-3,dim.tee1-3-dim.length/2);
-		stones[1].push(0.5,0.03,-0.05);
+		//stones[1].setPosition(-3,dim.tee1-3-dim.length/2);
+		//stones[1].push(0.1/*dir*/,0.1/*fps*/,-0.05);
+        stones[1].setPosition(.5,dim.tee1-3-dim.length/2);
 	}
 
+     var mouse = new BABYLON.Vector3(0,0,0)
+	
+     var onPointerDown = function (evt) {
+		mouse.x = scene.pointerX;
+		mouse.y = scene.pointerY;
+        mouse.down = true;
+    }
+
+    var onPointerUp = function () {
+        mouse.down = false;
+		camera.attachControl(canvas);
+    }
+
+    var onPointerMove = function (evt) {
+		if (mouse.down) {
+            camera.detachControl(canvas);
     
-    //window.addEventListener("click", function (evt) {
-    //    var pickResult = scene.pick(evt.clientX, evt.clientY);
-    //    console.log("pick-result: "+pickResult);
-    //});
-    
+            var dx = scene.pointerX-mouse.x;
+            var dy = scene.pointerY-mouse.y;
+            cameraPos.x -= dx*.01;
+            cameraPos.y += dy*.01;
+            if (cameraPos.y < 0.1) {
+                cameraPos.y = 0.1;
+            }
+            mouse.x = scene.pointerX;
+            mouse.y = scene.pointerY;
+        }
+    }
+	
+    canvas.addEventListener("pointerdown", onPointerDown, true);
+    canvas.addEventListener("pointerup", onPointerUp, true);
+    canvas.addEventListener("pointermove", onPointerMove, true);
+
