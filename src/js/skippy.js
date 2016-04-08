13b71e017741640462f5355b648c1c9f6610e483
Dani Jaggi <daniel.jaggi@aarenet.com>
2016-04-08 17:32:32 +0200

    adjust coordinates

--- src/js/skippy.js
+++ src/js/skippy.js
@@ -1,44 +1,40 @@
 
 	var dim = {
-		diameter12: 12,
-		diameterStone: 1,
-		width: 16,
-		center: 8,
-		length: 150,
-		hack1: 6,
-		back1: 12,
-		tee1: 18,
-		hog1: 39,
-		hog2: 111,
-		tee2: 132,
-		back2: 138,
-		hack2: 144,
-		man: 6
+		diameter12:     12,
+		diameterStone:  1,
+		width:          16,
+		center:         8,
+		length:         150,
+		man:            6,
+        offset:         6,
+		hack1:          0,   //6
+		back1:          6,   //12,
+		tee1:           12,  //18,
+		hog1:           33,  //39,
+		hog2:           105, //111,
+		tee2:           126, //132,
+		back2:          132, //138,
+		hack2:          138  //144,
 	};
-	
-	for (var p in dim) {
-		dim[p] = dim[p] * 1;
-	}
 	
 	var canvas = document.getElementById("renderCanvas");
 	var engine = new BABYLON.Engine(canvas, true);
 	var scene = new BABYLON.Scene(engine);
-	scene.clearColor = new BABYLON.Color3(0.4, 0.4, 0.5);
+	scene.clearColor = new BABYLON.Color3(0.6, 0.6, 0.9);
 	
-	var cameraPos = new BABYLON.Vector3(0,dim.man,6-dim.length/2);
+	var cameraPos = new BABYLON.Vector3(0,dim.man/3,dim.back2);
 	var camera = new BABYLON.FreeCamera("camera1", cameraPos, scene);
-	camera.setTarget(new BABYLON.Vector3(0,0,18-dim.length/2));
+	camera.setTarget(new BABYLON.Vector3(0,0,0));
 	camera.attachControl(canvas, false);
-
-	var hLight = new BABYLON.HemisphericLight("hLight", new BABYLON.Vector3(0, 10, 0), scene);
-	//hLight.intensity = 1;
-	//var dLight = new BABYLON.DirectionalLight("dLight", new BABYLON.Vector3(0,-10,0), scene);
-	//dLight.intensity = 0.9;
+    //camera.fov = 0.5;
+    
+	var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 10, 0), scene);
 	
 	var ground = BABYLON.Mesh.CreateGround("ground", dim.width, dim.length, 0, scene);
 	ground.material = new BABYLON.StandardMaterial("ground", scene);
 	ground.material.diffuseTexture = new BABYLON.Texture("images/rink.png", scene);
-	
+    ground.position = new BABYLON.Vector3(0,0,dim.length/2-dim.offset);
+		
 	var shadow = BABYLON.MeshBuilder.CreatePlane("shadow", {size:1.5}, scene);
 	shadow.material = new BABYLON.StandardMaterial("shadow", scene);
 	shadow.material.opacityTexture = new BABYLON.Texture("images/shadow.png", scene);
@@ -67,7 +63,7 @@
 		var that = this;
 		that.id = id; 
 		that.red = (id % 2)==0;
-		that.position = new BABYLON.Vector3(0,0.001+0.001*id,0);
+		that.position = new BABYLON.Vector3(0,0.001*(id+1),0);
 		that.visible = false;
 		that.state = { s:0, d:0, r:0 }
 		
@@ -106,8 +102,8 @@
 			}
             body.rotation.y = handle.rotation.y;
 			if (that.state.s>speedDelta) {
-				that.position.x -= Math.sin(that.state.d)*that.state.s*sec;
-				that.position.z -= Math.cos(that.state.d)*that.state.s*sec;
+				that.position.x += Math.sin(that.state.d)*that.state.s*sec;
+				that.position.z += Math.cos(that.state.d)*that.state.s*sec;
 				that.state.s -= speedDelta;
 			}
 			for (var p=id+1;p<16;p++) {
@@ -131,9 +127,9 @@
 			var dx = that.position.x-p.x;
 			var dz = that.position.z-p.z;
 			var dist = Math.sqrt(dx*dx+dz*dz);
-			if (dist<1) {
+			if (dist<=1) {
 				var a1 = Math.atan(dx/dz);
-				var a2 = a1-s.d;
+                var a2 = a1-s.d;
 				var ns1 = s.s*Math.sin(a2);
 				var nd1 = a1-Math.PI/2;
 				var nr1 = s.r*(1-rotationTakeover);
@@ -147,7 +143,6 @@
 				that.state.r = nr2;
 				
 				console.log("@@ nd1="+nd1+",   ns1="+ns1);
-				
 				return { d:nd1, s:ns1, r:nr1 };
 			}
 		}
@@ -162,25 +157,18 @@
 	BABYLON.SceneLoader.ImportMesh(undefined, "images/", "stone.babylon", scene, function (newMeshes) {
 		prototypeStone = newMeshes[0];
 		
-        //newMeshes[0] = BABYLON.MeshBuilder.CreateBox("box", {height: .5}, scene);
-    
-		var bi = prototypeStone.getBoundingInfo();
+        var bi = prototypeStone.getBoundingInfo();
 		var diameter = bi.maximum.x - bi.minimum.x;
 		var scale = dim.diameterStone/diameter;	
-        console.log("caluclated scale: "+scale);
         
 		prototypeStone.scaling = new BABYLON.Vector3(scale,scale,scale);
         prototypeStone.material = new BABYLON.StandardMaterial("stone", scene);
 		prototypeStone.material.diffuseTexture = new BABYLON.Texture("images/stone.png", scene);
         prototypeStone.material.diffuseTexture.vOffset = -0.05;
-        
         prototypeStone.material.backFaceCulling = false;
         prototypeStone.isVisible = false;
 	
-
-        //newMeshes[1] = BABYLON.MeshBuilder.CreateBox("box", {height: .5}, scene);
-    
-		prototypeYellowHandle = newMeshes[1];
+        prototypeYellowHandle = newMeshes[1];
 		prototypeYellowHandle.scaling = new BABYLON.Vector3(scale,scale,scale);
 		prototypeYellowHandle.material = new BABYLON.StandardMaterial("yellow", scene);
 		prototypeYellowHandle.material.diffuseColor = new BABYLON.Color3(1, 1, 0);
@@ -213,14 +201,31 @@
 	});
 	
 	function startup() {
-        camera.lockedTarget = stones[0];
+        camera.lockedTarget = new BABYLON.Vector3.Zero();
 
-		stones[0].setPosition(0,dim.tee1+20-dim.length/2);
+		/* OK */
+        stones[0].setPosition(0,dim.hog2);
 		stones[0].push(0/*dir*/,3.5/*fps*/,0.28);
-		
-		//stones[1].setPosition(-3,dim.tee1-3-dim.length/2);
-		//stones[1].push(0.1/*dir*/,0.1/*fps*/,-0.05);
-		stones[1].setPosition(.5,dim.tee1-3-dim.length/2);
+        stones[1].setPosition(-0.5,dim.tee2);
+        
+        /* WRONG 
+        stones[0].setPosition(2,dim.tee2);
+		stones[0].push(-Math.PI/2,1.2,0.08);
+        stones[1].setPosition(0,dim.tee2-.5);
+        */
+        
+        /* OK
+        stones[0].setPosition(2,dim.tee2);
+		stones[0].push(-Math.PI/2,1.2,0.08);
+        stones[1].setPosition(0,dim.tee2+.5);
+        */
+
+        /* WRONG
+        stones[0].setPosition(2,dim.tee2);
+		stones[0].push(-Math.PI/2,1.2,0.08);
+        stones[1].setPosition(-2,dim.tee2);
+		stones[1].push(Math.PI/2,1.2,0.08);
+        */
 	}
 
 	 var mouse = new BABYLON.Vector3(0,0,0)
@@ -242,8 +247,8 @@
 	
 			var dx = scene.pointerX-mouse.x;
 			var dy = scene.pointerY-mouse.y;
-            cameraPos.x -= dx*.01;
-			cameraPos.y += dy*.01;
+            cameraPos.x += dx*.02;
+			cameraPos.y += dy*.02;
 			if (cameraPos.y < 0.1) {
 				cameraPos.y = 0.1;
 			}
